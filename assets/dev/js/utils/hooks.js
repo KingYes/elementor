5e6203175e7ea74e977d82d65761a4694f0d4deb
Yakir Sitbon <kingyes1@gmail.com>
2018-08-13 15:11:25 +0300

    tests

--- assets/dev/js/utils/hooks.js
+++ assets/dev/js/utils/hooks.js
@@ -4,7 +4,7 @@
  * Handles managing all events for whatever you plug it into. Priorities for hooks are based on lowest to highest in
  * that, lowest priority hooks are fired first.
  */
-var EventManager = function() {
+var EventManager = function(){
 	var slice = Array.prototype.slice,
 		MethodsAvailable;
 
